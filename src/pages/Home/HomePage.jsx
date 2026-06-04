8f7906c8de1c7e8255599ba9dce83d94a1e3229d
Rishabh Tiwari <pt64632@gmail.com>
2026-06-04 18:12:29 +0530

    used async await

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -6,10 +6,12 @@ import "./HomePage.css";
 export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    axios.get("/api/products").then((response) => {
+  useEffect(  () => {
+    const getHomeData = async () => {
+    const response = await axios.get("/api/products");
       setProducts(response.data);
-    });
+    };
+    getHomeData();
   }, []);
   return (
     <>
